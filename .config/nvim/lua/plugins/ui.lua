f3cb688182d4bed11223adbbe999f7a0be90f655
Josué Martell <josue.martell17@gmail.com>
2024-02-22 01:06:08 -0600

    feat(nvim): added lualine

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -7,6 +7,11 @@ return {
 		},
 		opts = {
 			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
 				signature = {
 					enabled = false
 				},
@@ -16,4 +21,8 @@ return {
 			}
 		}
 	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = true
+	}
 }
