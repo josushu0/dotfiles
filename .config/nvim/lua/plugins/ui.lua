3e46c4715243e587599ece00d2f421c5fc55169b
Josué Martell <josue.martell17@gmail.com>
2024-04-24 22:55:12 -0600

    feat(nvim): added codesnap plugin

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"mistricky/codesnap.nvim",
+		version = "*",
+		build = "make",
+		opts = {
+			has_breadcrumbs = true,
+			bg_theme = "peach",
+			watermark = "",
+			save_path = "~/Pictures/Screenshots/",
+		},
+	},
+}
